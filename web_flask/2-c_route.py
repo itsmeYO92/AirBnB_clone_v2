ae510d5052583bc843b2843605de6c38fa432007
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-24 16:33:05 +0100

    variables in the routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' a script that starts a flask app '''
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' return hello HBNB! to the user '''
+    return ('Hello HBNB!')
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' return HBNB to the user '''
+    return ('HBNB')
+
+@app.route('/c/<text>', strict_slashes=False)
+def variable_route(text):
+    ''' return the C plus a custom text entred by the user '''
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
