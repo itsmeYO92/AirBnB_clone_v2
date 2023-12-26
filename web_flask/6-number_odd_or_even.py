82eae51268768621f715e1a0da2b091623cbb069
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-26 01:50:37 +0100

    odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+''' a script that starts a flask app '''
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' return hello HBNB! to the user '''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' return HBNB to the user '''
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def variable_route(text):
+    ''' return the C plus a custom text entred by the user '''
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    ''' return the C plus a custom text entred by the user '''
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def number(num):
+    ''' return the number entred by the user '''
+    return ("{} is number".format(num))
+
+
+@app.route('/number_template/<int:num>')
+def template_num(num):
+    if type(num) is int:
+        return render_template('5-number.html', num=num)
+
+
+@app.route('/number_odd_or_even/<int:num>')
+def odd_even(num):
+    if type(num) is int:
+        return render_template('6-number_odd_or_even.html', num=num)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
