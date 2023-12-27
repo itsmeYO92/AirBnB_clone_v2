fd4d683d279d8e1e95f826c4147e6dbec0c76699
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-27 19:17:14 +0100

    states

--- models/place.py
+++ models/place.py
@@ -9,9 +9,9 @@ class Place(BaseModel, Base):
     """ A place to stay """
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "places"
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathroms = Column(Integer, nullable=False, default=0)
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' a script that starts a flask app '''
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' return hello HBNB! to the user '''
+    return ('Hello HBNB!')
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    ''' return HBNB to the user '''
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
